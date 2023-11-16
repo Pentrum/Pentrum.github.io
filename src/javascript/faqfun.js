1a844daf9675cce158fa99c9ac7fdc34cfecd61d
PentrumStudios <pentrumstudios@proton.me>
2023-11-16 21:13:03 +0200

    GO WATCH BLUEY !!!!!!

---
+++ src/javascript/faqfun.js
@@ -0,0 +1,324 @@
+$(document).ready(function(){
+    $(".faqbox1").click(function(){
+        $(".faqbox2").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox2").css("opacity", "0");
+        $(".faqbox2").css("scale", "0.5");
+        $(".faqbox2").css("pointer-events", "none");
+
+        $(".faqbox3").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox3").css("opacity", "0");
+        $(".faqbox3").css("scale", "0.5");
+        $(".faqbox3").css("pointer-events", "none");
+
+        $(".faqbox4").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox4").css("opacity", "0");
+        $(".faqbox4").css("scale", "0.5");
+        $(".faqbox4").css("pointer-events", "none");
+
+        $(".faqbox5").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox5").css("opacity", "0");
+        $(".faqbox5").css("scale", "0.5");
+        $(".faqbox5").css("pointer-events", "none");
+
+        $(".faqbox6").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox6").css("opacity", "0");
+        $(".faqbox6").css("scale", "0.5");
+        $(".faqbox6").css("pointer-events", "none");
+
+
+        $(".faqbox1").css("transition", "width 0.7s ease-in-out 0.5s, height 0.7s ease-in-out 0.5s");
+        $(".faqbox1").css("width", "calc(100% - 2vmax)");
+        $(".faqbox1").css("height", "25vmax");
+        $(".faqtitle1").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle1").css("color", "var(--pri)");
+        $(".faqtitle1").css("opacity", "1");
+        $(".faqdesc1").css("transition", "opacity 0.3s ease 1.3s");
+        $(".faqdesc1").css("opacity", "1");
+
+
+        $(".faqexit").css("transition", "opacity 0.7s ease-in-out, box-shadow 0.3s ease, background 0.3s ease");
+        $(".faqexit").css("opacity", "1");
+    });
+    $(".faqbox2").click(function(){
+        $(".faqbox1").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox1").css("opacity", "0");
+        $(".faqbox1").css("scale", "0.5");
+        $(".faqbox1").css("pointer-events", "none");
+
+        $(".faqbox3").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox3").css("opacity", "0");
+        $(".faqbox3").css("scale", "0.5");
+        $(".faqbox3").css("pointer-events", "none");
+
+        $(".faqbox4").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox4").css("opacity", "0");
+        $(".faqbox4").css("scale", "0.5");
+        $(".faqbox4").css("pointer-events", "none");
+
+        $(".faqbox5").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox5").css("opacity", "0");
+        $(".faqbox5").css("scale", "0.5");
+        $(".faqbox5").css("pointer-events", "none");
+
+        $(".faqbox6").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox6").css("opacity", "0");
+        $(".faqbox6").css("scale", "0.5");
+        $(".faqbox6").css("pointer-events", "none");
+
+
+        $(".faqbox2").css("transition", "width 0.7s ease-in-out 0.5s, height 0.7s ease-in-out 0.5s");
+        $(".faqbox2").css("width", "calc(100% - 2vmax)");
+        $(".faqbox2").css("height", "25vmax");
+        $(".faqtitle2").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle2").css("color", "var(--pri)");
+        $(".faqtitle2").css("opacity", "1");
+        $(".faqdesc2").css("transition", "opacity 0.3s ease 1.3s");
+        $(".faqdesc2").css("opacity", "1");
+
+        $(".faqexit").css("transition", "opacity 0.7s ease-in-out, box-shadow 0.3s ease, background 0.3s ease");
+        $(".faqexit").css("opacity", "1");
+    });
+    $(".faqbox3").click(function(){
+        $(".faqbox1").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox1").css("opacity", "0");
+        $(".faqbox1").css("scale", "0.5");
+        $(".faqbox1").css("pointer-events", "none");
+
+        $(".faqbox2").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox2").css("opacity", "0");
+        $(".faqbox2").css("scale", "0.5");
+        $(".faqbox2").css("pointer-events", "none");
+
+        $(".faqbox4").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox4").css("opacity", "0");
+        $(".faqbox4").css("scale", "0.5");
+        $(".faqbox4").css("pointer-events", "none");
+
+        $(".faqbox5").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox5").css("opacity", "0");
+        $(".faqbox5").css("scale", "0.5");
+        $(".faqbox5").css("pointer-events", "none");
+
+        $(".faqbox6").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox6").css("opacity", "0");
+        $(".faqbox6").css("scale", "0.5");
+        $(".faqbox6").css("pointer-events", "none");
+
+
+        $(".faqbox3").css("transition", "width 0.7s ease-in-out 0.7s, height 0.7s ease-in-out 0.7s, top 0.3s ease-in-out 0.5s");
+        $(".faqbox3").css("width", "calc(100% - 2vmax)");
+        $(".faqbox3").css("height", "25vmax");
+        $(".faqbox3").css("top", "4vmax");
+        $(".faqtitle3").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle3").css("color", "var(--pri)");
+        $(".faqtitle3").css("opacity", "1");
+        $(".faqdesc3").css("transition", "opacity 0.3s ease 1.3s");
+        $(".faqdesc3").css("opacity", "1");
+
+        $(".faqexit").css("transition", "opacity 0.7s ease-in-out, box-shadow 0.3s ease, background 0.3s ease");
+        $(".faqexit").css("opacity", "1");
+    });
+    $(".faqbox4").click(function(){
+        $(".faqbox1").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox1").css("opacity", "0");
+        $(".faqbox1").css("scale", "0.5");
+        $(".faqbox1").css("pointer-events", "none");
+
+        $(".faqbox2").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox2").css("opacity", "0");
+        $(".faqbox2").css("scale", "0.5");
+        $(".faqbox2").css("pointer-events", "none");
+
+        $(".faqbox3").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox3").css("opacity", "0");
+        $(".faqbox3").css("scale", "0.5");
+        $(".faqbox3").css("pointer-events", "none");
+
+        $(".faqbox5").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox5").css("opacity", "0");
+        $(".faqbox5").css("scale", "0.5");
+        $(".faqbox5").css("pointer-events", "none");
+
+        $(".faqbox6").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox6").css("opacity", "0");
+        $(".faqbox6").css("scale", "0.5");
+        $(".faqbox6").css("pointer-events", "none");
+
+
+        $(".faqbox4").css("transition", "width 0.7s ease-in-out 0.7s, height 0.7s ease-in-out 0.7s, top 0.3s ease-in-out 0.5s");
+        $(".faqbox4").css("width", "calc(100% - 2vmax)");
+        $(".faqbox4").css("height", "25vmax");
+        $(".faqbox4").css("top", "4vmax");
+        $(".faqtitle4").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle4").css("color", "var(--pri)");
+        $(".faqtitle4").css("opacity", "1");
+        $(".faqdesc4").css("transition", "opacity 0.3s ease 1.3s");
+        $(".faqdesc4").css("opacity", "1");
+
+        $(".faqexit").css("transition", "opacity 0.7s ease-in-out, box-shadow 0.3s ease, background 0.3s ease");
+        $(".faqexit").css("opacity", "1");
+    });
+    $(".faqbox5").click(function(){
+        $(".faqbox1").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox1").css("opacity", "0");
+        $(".faqbox1").css("scale", "0.5");
+        $(".faqbox1").css("pointer-events", "none");
+
+        $(".faqbox2").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox2").css("opacity", "0");
+        $(".faqbox2").css("scale", "0.5");
+        $(".faqbox2").css("pointer-events", "none");
+
+        $(".faqbox3").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox3").css("opacity", "0");
+        $(".faqbox3").css("scale", "0.5");
+        $(".faqbox3").css("pointer-events", "none");
+
+        $(".faqbox4").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox4").css("opacity", "0");
+        $(".faqbox4").css("scale", "0.5");
+        $(".faqbox4").css("pointer-events", "none");
+
+        $(".faqbox6").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox6").css("opacity", "0");
+        $(".faqbox6").css("scale", "0.5");
+        $(".faqbox6").css("pointer-events", "none");
+
+
+        $(".faqbox5").css("transition", "width 0.7s ease-in-out 0.7s, height 0.7s ease-in-out 0.7s, top 0.3s ease-in-out 0.5s");
+        $(".faqbox5").css("width", "calc(100% - 2vmax)");
+        $(".faqbox5").css("height", "25vmax");
+        $(".faqbox5").css("top", "4vmax");
+        $(".faqtitle5").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle5").css("color", "var(--pri)");
+        $(".faqtitle5").css("opacity", "1");
+        $(".faqdesc5").css("transition", "opacity 0.3s ease 1.3s");
+        $(".faqdesc5").css("opacity", "1");
+
+        $(".faqexit").css("transition", "opacity 0.7s ease-in-out, box-shadow 0.3s ease, background 0.3s ease");
+        $(".faqexit").css("opacity", "1");
+    });
+    $(".faqbox6").click(function(){
+        $(".faqbox1").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox1").css("opacity", "0");
+        $(".faqbox1").css("scale", "0.5");
+        $(".faqbox1").css("pointer-events", "none");
+
+        $(".faqbox2").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox2").css("opacity", "0");
+        $(".faqbox2").css("scale", "0.5");
+        $(".faqbox2").css("pointer-events", "none");
+
+        $(".faqbox3").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox3").css("opacity", "0");
+        $(".faqbox3").css("scale", "0.5");
+        $(".faqbox3").css("pointer-events", "none");
+
+        $(".faqbox4").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox4").css("opacity", "0");
+        $(".faqbox4").css("scale", "0.5");
+        $(".faqbox4").css("pointer-events", "none");
+
+        $(".faqbox5").css("transition", "scale 0.8s ease, opacity 0.2s ease");
+        $(".faqbox5").css("opacity", "0");
+        $(".faqbox5").css("scale", "0.5");
+        $(".faqbox5").css("pointer-events", "none");
+
+
+        $(".faqbox6").css("transition", "width 0.7s ease-in-out 0.7s, height 0.7s ease-in-out 0.7s, top 0.3s ease-in-out 0.5s");
+        $(".faqbox6").css("width", "calc(100% - 2vmax)");
+        $(".faqbox6").css("height", "25vmax");
+        $(".faqbox6").css("top", "4vmax");
+        $(".faqtitle6").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle6").css("color", "var(--pri)");
+        $(".faqtitle6").css("opacity", "1");
+        $(".faqdesc6").css("transition", "opacity 0.3s ease 1.3s");
+        $(".faqdesc6").css("opacity", "1");
+
+        $(".faqexit").css("transition", "opacity 0.7s ease-in-out, box-shadow 0.3s ease, background 0.3s ease");
+        $(".faqexit").css("opacity", "1");
+    });
+    $(".faqexit").click(function(){
+        $(".faqbox1").css("transition", "width 0.7s ease-in-out, height 0.7s ease-in-out, top 0.3s ease-in-out, opacity 0.7s ease 0.7s, scale 0.5s ease 0.7s");
+        $(".faqbox1").css("width", "calc(50% - 1.25vmax)");
+        $(".faqbox1").css("height", "8vmax");
+        $(".faqbox1").css("top", "4vmax");
+        $(".faqbox1").css("opacity", "1");
+        $(".faqbox1").css("scale", "1");
+        $(".faqbox1").css("pointer-events", "auto");
+        $(".faqtitle1").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle1").css("color", "var(--txt)");
+        $(".faqtitle1").css("opacity", "0.5");
+        $(".faqdesc1").css("transition", "opacity 0.15s ease");
+        $(".faqdesc1").css("opacity", "0");
+
+        $(".faqbox2").css("transition", "width 0.7s ease-in-out, height 0.7s ease-in-out, top 0.3s ease-in-out, opacity 0.7s ease 0.7s, scale 0.5s ease 0.7s");
+        $(".faqbox2").css("width", "calc(50% - 1.25vmax)");
+        $(".faqbox2").css("height", "8vmax");
+        $(".faqbox2").css("top", "4vmax");
+        $(".faqbox2").css("opacity", "1");
+        $(".faqbox2").css("scale", "1");
+        $(".faqbox2").css("pointer-events", "auto");
+        $(".faqtitle2").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle2").css("color", "var(--txt)");
+        $(".faqtitle2").css("opacity", "0.5");
+        $(".faqdesc2").css("transition", "opacity 0.15s ease");
+        $(".faqdesc2").css("opacity", "0");
+
+        $(".faqbox3").css("transition", "width 0.7s ease-in-out, height 0.7s ease-in-out, top 0.3s ease-in-out 0.5s, opacity 0.7s ease 0.7s, scale 0.5s ease 0.7s");
+        $(".faqbox3").css("width", "calc(50% - 1.25vmax)");
+        $(".faqbox3").css("height", "8vmax");
+        $(".faqbox3").css("top", "12.5vmax");
+        $(".faqbox3").css("opacity", "1");
+        $(".faqbox3").css("scale", "1");
+        $(".faqbox3").css("pointer-events", "auto");
+        $(".faqtitle3").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle3").css("color", "var(--txt)");
+        $(".faqtitle3").css("opacity", "0.5");
+        $(".faqdesc3").css("transition", "opacity 0.15s ease");
+        $(".faqdesc3").css("opacity", "0");
+
+        $(".faqbox4").css("transition", "width 0.7s ease-in-out, height 0.7s ease-in-out, top 0.3s ease-in-out 0.5s, opacity 0.7s ease 0.7s, scale 0.5s ease 0.7s");
+        $(".faqbox4").css("width", "calc(50% - 1.25vmax)");
+        $(".faqbox4").css("height", "8vmax");
+        $(".faqbox4").css("top", "12.5vmax");
+        $(".faqbox4").css("opacity", "1");
+        $(".faqbox4").css("scale", "1");
+        $(".faqbox4").css("pointer-events", "auto");
+        $(".faqtitle4").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle4").css("color", "var(--txt)");
+        $(".faqtitle4").css("opacity", "0.5");
+        $(".faqdesc4").css("transition", "opacity 0.15s ease");
+        $(".faqdesc4").css("opacity", "0");
+
+        $(".faqbox5").css("transition", "width 0.7s ease-in-out, height 0.7s ease-in-out, top 0.3s ease-in-out 0.5s, opacity 0.7s ease 0.7s, scale 0.5s ease 0.7s");
+        $(".faqbox5").css("width", "calc(50% - 1.25vmax)");
+        $(".faqbox5").css("height", "8vmax");
+        $(".faqbox5").css("top", "21vmax");
+        $(".faqbox5").css("opacity", "1");
+        $(".faqbox5").css("scale", "1");
+        $(".faqbox5").css("pointer-events", "auto");
+        $(".faqtitle5").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle5").css("color", "var(--txt)");
+        $(".faqtitle5").css("opacity", "0.5");
+        $(".faqdesc5").css("transition", "opacity 0.15s ease");
+        $(".faqdesc5").css("opacity", "0");
+
+        $(".faqbox6").css("transition", "width 0.7s ease-in-out, height 0.7s ease-in-out, top 0.3s ease-in-out 0.5s, opacity 0.7s ease 0.7s, scale 0.5s ease 0.7s");
+        $(".faqbox6").css("width", "calc(50% - 1.25vmax)");
+        $(".faqbox6").css("height", "8vmax");
+        $(".faqbox6").css("top", "21vmax");
+        $(".faqbox6").css("opacity", "1");
+        $(".faqbox6").css("scale", "1");
+        $(".faqbox6").css("pointer-events", "auto");
+        $(".faqtitle6").css("transition", "color 0.3s ease, opacity 0.3s ease");
+        $(".faqtitle6").css("color", "var(--txt)");
+        $(".faqtitle6").css("opacity", "0.5");
+        $(".faqdesc6").css("transition", "opacity 0.15s ease");
+        $(".faqdesc6").css("opacity", "0");
+
+        $(".faqexit").css("transition", "opacity 0.7s ease-in-out");
+        $(".faqexit").css("opacity", "0");
+    });
+
+});
